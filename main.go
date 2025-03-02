11915a2f76e11228e8ab6a82feb6e2a967e5f690
jomla97 <jomla97@gmail.com>
2025-03-02 19:11:01 +0100

    Implemented POST /log endpoint

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var client *mongo.Client
+
+func main() {
+	c, err := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		panic(err)
+	}
+	client = c
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	r := gin.Default()
+	r.GET("/ping", ping)
+	r.POST("/log", postLog)
+	r.Run(":80")
+}
